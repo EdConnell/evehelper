3fbc0bdfd85cd0324dc2885326510b305604ec60
EdConnell <edwardconnell@gmail.com>
2014-04-11 11:09:18 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evehelper::Application.config.secret_token = '1349f59047908cf093890e410ea2ac0d19e5d7b6640f903ea8ce08223dd05322865683c4d6562a2edd15233e6c99f6ba6fd8496773d353cad84ac72c8daef6f4'
