dcc78ea2a11b4a584309bb144b5de50075fc89cd
EdConnell <edwardconnell@gmail.com>
2014-07-01 15:56:16 -0500

    Added x and y values for solar systems

--- db/migrate/20140416182943_create_solar_systems.rb
+++ db/migrate/20140416182943_create_solar_systems.rb
@@ -5,6 +5,8 @@ class CreateSolarSystems < ActiveRecord::Migration
       t.references :region
       t.string :eve_id
       t.boolean :station, :default => "F"
+      t.integer :x
+      t.integer :y
     end
     add_index :solar_systems, :region_id
   end
